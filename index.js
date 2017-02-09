796184fe77901bcd7575965b3e77b0e82f979b75
Dave Wasmer <davewasmer@gmail.com>
2017-02-09 10:50:21 -0700

    Add includes, sort, configName options. Change return value to array of objects with plugin directories and package.json contents

--- index.js
+++ index.js
@@ -1,54 +1,91 @@
 'use strict';
 const fs = require('fs');
 const path = require('path');
+const DAG = require('dag-map').default;
 
 module.exports = function(options) {
   options = options || {};
+  // The node_modules directory to scan for plugins
   let modulesDir = options.modulesDir || path.join(process.cwd(), 'node_modules');
+  // The path to the package.json that lists dependencies to check for plugins
   let pkgPath = options.pkg || 'package.json';
+  let pkg = require(pkgPath);
+  // An array of additional paths to check as plugins
+  let includes = options.include || [];
+  // If supplied, a package will be considered a plugin if `keyword` is present in it's
+  // package.json "keywords" array
   let keyword = options.keyword;
+  // If sort: true is supplied, this determines what property of the plugin's package.json to
+  // check for the sort configuration (it should be an object with "before" and "after" properties
+  // which are arrays of other plugins names)
+  let configName = options.configName || pkg.name;
 
-  function isPlugin(pkg) {
+  let pluginCandidateDirectories = [];
+
+  // The filter function that determines whether a package is a plugin. If options.filter
+  // is supplied, go with that. Otherwise, check for options.keyword match.
+  function isPlugin(plugin) {
     if (options.filter) {
-      return options.filter(pkg);
+      return options.filter(plugin);
     }
-    if (!pkg.keywords) {
-      return;
+    if (!plugin.pkg.keywords) {
+      return false;
     }
     if (!keyword) {
       keyword = require(pkgPath).name;
     }
-    return pkg.keywords.indexOf(keyword) > -1;
+    return plugin.pkg.keywords.indexOf(keyword) > -1;
   }
 
   // scanAllDirs indicates that we should ignore the package.json contents and
   // simply look at the contents of the node_modules directory
   if (options.scanAllDirs) {
-    return fs.readdirSync(modulesDir)
+    pluginCandidateDirectories = fs.readdirSync(modulesDir)
       .filter((name) => name !== '.bin')
       .map((name) => path.join(modulesDir, name))
-      .filter((dir) => fs.statSync(dir).isDirectory())
-      .map((dir) => require(path.join(dir, 'package.json')))
-      .filter(isPlugin)
-      .map((pkg) => pkg.name);
+      .filter((dir) => fs.statSync(dir).isDirectory());
+  // Otherwise, use the consuming package.json dependencies as the list of plugin candidates
+  } else {
+    let dependencies = [];
+    if (!options.excludeDependencies) {
+      dependencies = dependencies.concat(Object.keys(pkg.dependencies || {}));
+    }
+    if (options.includeDev) {
+      dependencies = dependencies.concat(Object.keys(pkg.devDependencies || {}));
+    }
+    if (options.includePeer) {
+      dependencies = dependencies.concat(Object.keys(pkg.peerDependencies || {}));
+    }
+    if (options.includeBundle) {
+      dependencies = dependencies.concat(Object.keys(pkg.bundleDependencies || pkg.bundledDependencies || {}));
+    }
+    if (options.includeOptional) {
+      dependencies = dependencies.concat(Object.keys(pkg.optionalDependencies || {}));
+    }
+    pluginCandidateDirectories = dependencies.map((dir) => path.join(modulesDir, dir));
   }
 
-  let pkg = require(pkgPath);
-  let dependencies = Object.keys(pkg.dependencies || {});
-  if (!options.ignoreDevDependencies) {
-    dependencies = dependencies.concat(Object.keys(pkg.devDependencies || {}));
-  }
-  if (!options.ignorePeerDependencies) {
-    dependencies = dependencies.concat(Object.keys(pkg.peerDependencies || {}));
-  }
-  if (!options.ignoreBundleDependencies) {
-    dependencies = dependencies.concat(Object.keys(pkg.bundleDependencies || pkg.bundledDependencies || {}));
-  }
-  if (!options.ignoreOptionalDependencies) {
-    dependencies = dependencies.concat(Object.keys(pkg.optionalDependencies || {}));
+  // Include an manually specified packages in the list of plugin candidates
+  pluginCandidateDirectories = pluginCandidateDirectories.concat(includes);
+
+  let plugins = pluginCandidateDirectories.map((dir) => {
+    return {
+      dir: dir,
+      pkg: require(path.join(dir, 'package.json'))
+    };
+  }).filter(isPlugin);
+
+  if (options.sort) {
+    let graph = new DAG();
+    plugins.forEach((plugin) => {
+      let pluginConfig = plugin.pkg[options.configName];
+      graph.add(plugin.pkg.name, plugin, pluginConfig.before, pluginConfig.after);
+    });
+    plugins = [];
+    graph.topsort((key, value) => {
+      plugins.push(value);
+    });
   }
 
-  return dependencies.map((dir) => require(path.join(modulesDir, dir, 'package.json')))
-    .filter(isPlugin)
-    .map((_pkg) => _pkg.name);
-};
+  return plugins;
+}
--- test/index.js
+++ test/index.js
@@ -1,51 +1,73 @@
-path = require('path');
-assert = require('assert');
-findPlugins = require('../index');
+var path = require('path');
+var assert = require('assert');
+var findPlugins = require('../index');
+
+function didFindPlugin(plugins, pluginName) {
+  return assert(plugins.find(function(plugin) { return plugin.pkg.name === pluginName }), 'missing plugin ' + pluginName);
+}
+function didNotFindPlugin(plugins, pluginName) {
+  return assert(!plugins.find(function(plugin) { return plugin.pkg.name === pluginName }), 'extraneous pluginj ' + pluginName);
+}
+
+var fixtureModules = path.join(__dirname, 'fixtures', 'app', 'node_modules');
+var fixturePackage = path.join(__dirname, 'fixtures', 'app', 'package.json');
 
 describe('find-plugins', function(){
 
   it('should find plugins found in package.json', function(){
-    plugins = findPlugins({
-      modulesDir: path.join(__dirname, 'fixtures', 'app', 'node_modules'),
-      pkg: path.join(__dirname, 'fixtures', 'app', 'package.json'),
+    plugins = findPlugins({ modulesDir: fixtureModules, pkg: fixturePackage,
       keyword: 'plugin'
     });
-    assert(plugins.indexOf('foobar') > -1);
-    assert(plugins.indexOf('extra-foobar') === -1);
-    assert(plugins.indexOf('not-a-plugin') === -1);
+    didFindPlugin(plugins, 'foobar');
+    didNotFindPlugin(plugins, 'extra-foobar');
+    didNotFindPlugin(plugins, 'not-a-plugin');
   });
 
   it('should find plugins ignoring package.json', function(){
-    plugins = findPlugins({
-      modulesDir: path.join(__dirname, 'fixtures', 'app', 'node_modules'),
-      pkg: path.join(__dirname, 'fixtures', 'app', 'package.json'),
+    plugins = findPlugins({ modulesDir: fixtureModules, pkg: fixturePackage,
       keyword: 'plugin',
       scanAllDirs: true
     });
-    assert(plugins.indexOf('foobar') > -1);
-    assert(plugins.indexOf('extra-foobar') > -1);
-    assert(plugins.indexOf('not-a-plugin') === -1);
+    didFindPlugin(plugins, 'foobar');
+    didFindPlugin(plugins, 'extra-foobar');
+    didNotFindPlugin(plugins, 'not-a-plugin');
   });
 
   it('should find plugins using a custom filter', function(){
-    plugins = findPlugins({
-      modulesDir: path.join(__dirname, 'fixtures', 'app', 'node_modules'),
-      pkg: path.join(__dirname, 'fixtures', 'app', 'package.json'),
-      keyword: 'plugin',
+    plugins = findPlugins({ modulesDir: fixtureModules, pkg: fixturePackage,
       scanAllDirs: true,
-      filter: function(pkg) { return pkg.name === 'not-a-plugin' }
+      filter: function(plugin) { return plugin.pkg.name === 'not-a-plugin' }
     });
-    assert(plugins.indexOf('foobar') === -1);
-    assert(plugins.indexOf('extra-foobar') === -1);
-    assert(plugins.indexOf('not-a-plugin') > -1);
+    didNotFindPlugin(plugins, 'foobar');
+    didNotFindPlugin(plugins, 'extra-foobar');
+    didFindPlugin(plugins, 'not-a-plugin');
   });
 
   it('should use the package name if no keyword is supplied', function(){
-    plugins = findPlugins({
-      modulesDir: path.join(__dirname, 'fixtures', 'app', 'node_modules'),
-      pkg: path.join(__dirname, 'fixtures', 'app', 'package.json')
+    plugins = findPlugins({ modulesDir: fixtureModules, pkg: fixturePackage });
+    didFindPlugin(plugins, 'foobar');
+  });
+
+  it('should include any directories manually specified by "include"', function(){
+    plugins = findPlugins({ modulesDir: fixtureModules, pkg: fixturePackage,
+      keyword: 'plugin',
+      include: [ path.join(__dirname, 'fixtures', 'app', 'non-npm-plugin') ]
     });
-    assert(plugins.indexOf('foobar') > -1);
+    didFindPlugin(plugins, 'foobar');
+    didNotFindPlugin(plugins, 'extra-foobar');
+    didNotFindPlugin(plugins, 'not-a-plugin');
+    didFindPlugin(plugins, 'non-npm-plugin');
+  });
+
+  it('should sort plugins via a DAG of dependencies when "sort" is true', function() {
+    plugins = findPlugins({ modulesDir: fixtureModules, pkg: fixturePackage,
+      scanAllDirs: true,
+      filter: function(plugin) { return true; },
+      sort: true,
+      configName: "plugin-config"
+    });
+    pluginNames = plugins.map(function(plugin) { return plugin.pkg.name });
+    assert.deepEqual(pluginNames, [ 'foobar', 'extra-foobar', 'not-a-plugin' ], 'plugins are incorrectly sorted');
   });
 
 });
